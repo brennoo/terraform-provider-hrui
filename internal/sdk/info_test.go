751dcb2afd00801427ee597cfa4c3a6bf7b68d51
Brenno Oliveira <brenno.oliveira@deliveryhero.com>
2025-02-16 23:54:02 +0100

    chore: add missing tests
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>

---
+++ internal/sdk/info_test.go
@@ -0,0 +1,68 @@
+package sdk
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetSystemInfo(t *testing.T) {
+	tests := []struct {
+		name       string
+		html       string
+		expected   map[string]string
+		shouldFail bool
+		httpStatus int
+	}{
+		{
+			"Valid system info",
+			`<table>
+				<tr><th>OS</th><td>Linux</td></tr>
+				<tr><th>Version</th><td>1.2.3</td></tr>
+			</table>`,
+			map[string]string{
+				"OS":      "Linux",
+				"Version": "1.2.3",
+			},
+			false,
+			http.StatusOK,
+		},
+		{
+			"Empty table",
+			`<table></table>`,
+			map[string]string{},
+			false,
+			http.StatusOK,
+		},
+		{
+			"Request error",
+			"",
+			nil,
+			true,
+			http.StatusInternalServerError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tt.httpStatus)
+				fmt.Fprintln(w, tt.html)
+			}))
+			defer server.Close()
+
+			client := &HRUIClient{URL: server.URL, HttpClient: server.Client()}
+			resp, err := client.Request("GET", server.URL, nil, nil)
+
+			if tt.shouldFail {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, resp)
+			}
+		})
+	}
+}
---
+++ internal/sdk/ip_test.go
@@ -0,0 +1,100 @@
+package sdk
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetIPAddressSettings(t *testing.T) {
+	tests := []struct {
+		name       string
+		html       string
+		expected   *IPAddressSettings
+		shouldFail bool
+		httpStatus int
+	}{
+		{
+			"Valid IP settings",
+			`<select name='dhcp_state'>
+				<option value='0' selected>Static</option>
+			</select>
+			<input name='ip' value='192.168.1.100'>
+			<input name='netmask' value='255.255.255.0'>
+			<input name='gateway' value='192.168.1.1'>`,
+			&IPAddressSettings{false, "192.168.1.100", "255.255.255.0", "192.168.1.1"},
+			false,
+			http.StatusOK,
+		},
+		{
+			"Request error",
+			"",
+			nil,
+			true,
+			http.StatusInternalServerError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tt.httpStatus)
+				fmt.Fprintln(w, tt.html)
+			}))
+			defer server.Close()
+
+			client := &HRUIClient{URL: server.URL, HttpClient: server.Client()}
+			resp, err := client.Request("GET", server.URL, nil, nil)
+
+			if tt.shouldFail {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, resp)
+			}
+		})
+	}
+}
+
+func TestSetIPAddressSettings(t *testing.T) {
+	tests := []struct {
+		name       string
+		settings   *IPAddressSettings
+		httpStatus int
+		shouldFail bool
+	}{
+		{
+			"Successful update",
+			&IPAddressSettings{false, "192.168.1.200", "255.255.255.0", "192.168.1.1"},
+			http.StatusOK,
+			false,
+		},
+		{
+			"Request error",
+			&IPAddressSettings{false, "192.168.1.200", "255.255.255.0", "192.168.1.1"},
+			http.StatusInternalServerError,
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tt.httpStatus)
+			}))
+			defer server.Close()
+
+			client := &HRUIClient{URL: server.URL, HttpClient: server.Client()}
+			err := client.SetIPAddressSettings(tt.settings)
+
+			if tt.shouldFail {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
